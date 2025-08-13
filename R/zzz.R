c01bf01f3c8f766f8a3df9df7103179f34c12bcb
lukakoning <102363211+lukakoning@users.noreply.github.com>
2025-08-13 09:35:04 +0200

    Update R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -11,17 +11,17 @@ cli::cli_h3(
   )
 )
 cli::cli_alert_info(
-  "You may download the latest version from {.href [GitHub](https://github.com/tjarkvandemerwe/tidyprompt)}, using:"
+  "You may download the latest version from {.href [GitHub](https://github.com/KennispuntTwente/tidyprompt)}, using:"
 )
 cli::cli_text(
-  "{cli::symbol$arrow_right} {.run [remotes::install_github()](remotes::install_github(\"tjarkvandemerwe/tidyprompt\"))}"
+  "{cli::symbol$arrow_right} {.run [remotes::install_github()](remotes::install_github(\"KennispuntTwente/tidyprompt\"))}"
 )
 cli::cli_alert_info("Bugs, suggestions, questions, or contributions?")
 cli::cli_text(
   paste0(
     "{cli::symbol$arrow_right}  Open an",
-    " {.href [issue](https://github.com/tjarkvandemerwe/tidyprompt/issues)}",
-    " or {.href [pull request](https://github.com/tjarkvandemerwe/tidyprompt/pulls)}"
+    " {.href [issue](https://github.com/KennispuntTwente/tidyprompt/issues)}",
+    " or {.href [pull request](https://github.com/KennispuntTwente/tidyprompt/pulls)}"
   )
 )
 cat("\n")
